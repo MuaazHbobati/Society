f436b3d10772971fc384f1e287eb0eaba9fff81e
MuaazHbobati <moaazhbobate@gmail.com>
2026-02-18 06:28:54 +0400

    Remove Angular frontend folder — migrating to React for the new frontend

--- society-frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./features/home/pages/home-page/home-page')
-      .then(m => m.HomePageComponent)
-  },
-];
