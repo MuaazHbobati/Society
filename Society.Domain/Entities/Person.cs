d8457a9f8e4f41a9bde867b97ada06133757b1dd
MuaazHbobati <moaazhbobate@gmail.com>
2026-03-23 05:04:02 +0400

    Delete FatherName from person entity

--- Society.Domain/Entities/Person.cs
+++ Society.Domain/Entities/Person.cs
@@ -14,10 +14,7 @@ namespace Society.Domain.Entities
         public int Id { get; set; }
 
         [Required, MaxLength(100)]
-        public string FirstName { get; set; } = null;
-        
-        [Required, MaxLength(100)]
-        public string FatherName { get; set; } = null;
+        public string FirstName { get; set; } = null;             
 
         [Required, MaxLength(100)]
         public string LastName { get; set; } = null;
--- Society.Domain/obj/Debug/net8.0/Society.Domain.AssemblyInfo.cs
+++ Society.Domain/obj/Debug/net8.0/Society.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Society.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f2f4ae7a182752b430320419bd888882dd97d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4caaafe46b75b4fb708bbed4c7f04b5b99b57f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Society.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Society.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
